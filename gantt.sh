fcc8cde6b62d46db5982af3c7f31b3b4b0935a81
deanturpin <git@lvbtn.uk>
2017-02-01 19:01:11 +0000

    index properly and create summary

--- gantt.sh
+++ gantt.sh
@@ -5,10 +5,17 @@ declare -A tasks
 tasks["one"]=4
 tasks["two"]=7
 tasks["three"]=10
+tasks["four"]=10
+tasks["five"]=10
+tasks["six"]=10
 
 # Clear the screen
 clear
 
+# Start week
+start="1 feb 2017"
+
+# Print tasks
 y=1
 indent=0
 for key in ${!tasks[*]}; do
@@ -18,10 +25,19 @@ for key in ${!tasks[*]}; do
 	length=${tasks[$key]}
 	(( indent += $length ))
 
-	for (( i=0; i < $length; ++i)); do bar+='#'; done
+	for (( i=0; i < $length; ++i)); do bar+='-'; done
 
 	echo -en "$bar $key"
 	tput cup $y $indent
 
 	(( ++y ))
 done
+
+# Calcuate number of weeks
+tput cup $y 0
+readonly weeks=$(( indent / 5 ))
+
+# Print summary
+echo Total weeks $weeks
+echo $(date --date="$start")
+echo $(date --date="$start" -d "$weeks weeks")
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
 Simple command line Gantt chart.
+
+```bash
+---------- four
+          ---- one
+              ---------- five
+                        ---------- six
+                                  ------- two
+                                         ---------- three
+
+Total weeks 10
+Wed 1 Feb 00:00:00 GMT 2017
+Wed 12 Apr 19:58:50 BST 2017
+```
