02fba02b0290fc5f2d7d43736c0b5305132e2e53
deanturpin <git@lvbtn.uk>
2017-02-01 18:41:17 +0000

    first implementation

--- gantt.sh
+++ gantt.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo gantt
+declare -A tasks
+
+tasks["one"]=4
+tasks["two"]=7
+tasks["three"]=10
+
+# Clear the screen
+clear
+
+y=1
+indent=0
+for key in ${!tasks[*]}; do
+
+	# Create bar
+	bar=''
+	length=${tasks[$key]}
+	(( indent += $length ))
+
+	for (( i=0; i < $length; ++i)); do bar+='#'; done
+
+	echo -en "$bar $key"
+	tput cup $y $indent
+
+	(( ++y ))
+done
