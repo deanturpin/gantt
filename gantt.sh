035f18efede0a8dabb0919213951dfb3b6a287ff
Dean Turpin <github@lvbtn.uk>
2017-02-02 11:06:24 +0000

    import

--- gantt.sh
+++ gantt.sh
@@ -6,8 +6,11 @@ start="1 feb 2017"
 # Read tasks from stdin
 mapfile tasks
 
-# Initialise total days
+# Initialise total days and task count
 days=0
+count=0
+
+clear
 
 # Iterate over tasks
 for (( i = 0; i < ${#tasks[*]}; ++i )); do 
@@ -15,8 +18,9 @@ for (( i = 0; i < ${#tasks[*]}; ++i )); do
 	task=${tasks[i]}
 
 	# Only print bar if we can extract a duration
-	# if [[ $task =~ ^(.*)\ +([0-9]+)$ ]]; then
-	if [[ $task =~ (.*)\ +([[:digit:]]+) ]]; then
+	if [[ $task =~ ([^\#]*)\ +([[:digit:]]+) ]]; then
+
+        (( ++count ))
 
 		# Store title and duration
 		title=${BASH_REMATCH[1]}
@@ -25,10 +29,11 @@ for (( i = 0; i < ${#tasks[*]}; ++i )); do
 		# Construct task bar
 		bar='|'
 		for (( j = 0; j < $days; ++j )); do bar+=" "; done
-		for (( j = 0; j < $duration; ++j )); do bar+="#"; done
+		for (( j = 0; j < $(( duration-1 )); ++j )); do bar+="-"; done
+		bar+='|'
 
 		# Print bar and title
-		echo "$bar $(( i+1 )). $title"
+		echo "$bar" $count. $title "($duration days)"
 
 		# Update days by current task length
 		(( days += duration ))
@@ -38,13 +43,8 @@ done
 # Calcuate number of weeks
 readonly weeks=$(( days/5 ))
 
-# Print bottom bar
-echo -n '|'
-for (( j = 0; j < $(( weeks+3 )); ++j )); do echo -n "_____"; done
-echo
-
 # Print summary
-echo -e "\nTasks ${#tasks[*]}"
+echo -e "\nTasks $count"
 echo Weeks $weeks
 echo Start $(date +"%d %b %Y" --date="$start")
 echo Compl $(date +"%d %b %Y" --date="$start" -d "$weeks weeks")
